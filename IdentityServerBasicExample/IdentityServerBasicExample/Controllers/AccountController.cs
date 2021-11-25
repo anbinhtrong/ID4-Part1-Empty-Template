7759e31a94a7b9501603c3dd3b5138031033a8a0
Minh Tam <minhtam.seta@gmail.com>
2021-11-25 23:44:38 +0700

    Add claims

---
+++ IdentityServerBasicExample/IdentityServerBasicExample/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace IdentityServerBasicExample.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "AnAnh"),
+                new Claim(ClaimTypes.Email, "ananh@test.com")
+            };
+            var demoIdentity = new ClaimsIdentity(claims, "Demo Identity");
+            var userPrinciple = new ClaimsPrincipal(demoIdentity);
+            HttpContext.SignInAsync(userPrinciple);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
